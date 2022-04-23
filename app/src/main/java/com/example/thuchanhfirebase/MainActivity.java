14fae57d1f29f679095f648a74a414ebcc133b53
Quanost <nguyenquanost3@gmail.com~>
2022-04-23 22:38:39 +0700

    UI

---
+++ app/src/main/java/com/example/thuchanhfirebase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.thuchanhfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_screen);
+    }
+}
